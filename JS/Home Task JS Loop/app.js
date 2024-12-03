749674026489c047c9d42bb9b257f26f2a42f009
Rauf <104986212+RaufAhmadov@users.noreply.github.com>
2024-12-03 21:36:20 +0400

    For Loop ,Tarla folder and foodwagon
    Updated and created

--- JS/Home Task JS Loop/app.js
+++ JS/Home Task JS Loop/app.js
@@ -1,48 +1,62 @@
 // Task-01
-// İkirəqəmli ədədi üçrəqəmli olana qədər artırın.
 // İstifadəçidən ikirəqəmli ədəd daxil etməsini xahiş edin.
+// İkirəqəmli ədədi üçrəqəmli olana qədər artırın.
 // Bu rəqəmi üçrəqəmli olana kimi 7 - ədədi qədər artırın.
 // Son dəyəri konsola çıxarın
 
-// let num = +prompt("İkirəqəmli ədəd daxil edin");
+// let num = +prompt("İkirəqəmli ədəd daxil edin:");
+// if (num >= 10 && num < 100) { 
+//     for (let i = num; ; i += 7) { 
+//         if (i >= 100) {
+//             console.log("Üçrəqəmli rəqəm:", i); 
+//             break;
+//         }
+//     }
+// }else {console.log("Xahiş edirik, yalnız ikirəqəmli ədəd daxil edin.");}
 
-// if (num >= 10 && num <= 99) {
-//     for (let i = 0; num < 100; num += 7) {
-//         console.log(num);
-//     }
-//     if (num >= 100) {
-//         console.log(num);
-//     }
-// }
 
 
 // Task-02
 // Konsola N dəfə «I know how to use cycles» mesajı çıxaran proqram yazın.
 // Proqram N ədədini istifadəçidən soruşur.
 
-// let num = +prompt();
+// let num = 25;
+// let word = "I know how to use cycles";
 
-// for(let i = 0; i < num; i++){
-//     console.log("I know how to use cycles");
+// for (let i = 0; i < num; i++) {
+//     console.log(word);
 // }
 
 
 // Task-03
 // Sonu 0 ilə bitən bütün üçrəqəmli ədədləri konsola çıxaran proqram yazın.
 
-// for(let i = 100; i <= 999; i++){
-//     if(i % 10 === 0){
-//         console.log(i);
-//     }
-// }
+// let num = +prompt("100 ve 990 diapazonunda reqem yaz");
+// if (num >= 100 && num <= 990) {
+    
+//     for (let i = num; i <= 990 ; i++) {
+//         if(i % 10 === 0){
+//             console.log(i);
+//         }
+//     };
+// }else { console.log("reqem diapazonu 100 ve 990 arasidir.");}
+
 
 // Task-04
 // Bütün ikirəqəmli tək ədədlərin cəmini konsola çıxaran proqram yazın.
 
-// let total = 0;
-// for(let i = 10; i <= 99; i++){
-//     total = total + i;
-// }console.log(total);
+// let num = +prompt();
+
+// if (num >= 10 && num <= 99) {
+//     let sum = 0;
+//     for (let i = num; i <= 99; i++ ) {
+//         if (i % 2 !== 0) {
+//             console.log(i);
+//             sum = sum + i;
+//         }
+//     };console.log(`Tek ededlerin cemi ${sum}`);
+// }else{console.log("Get tezden gelersen!!");}
+
 
 
 // Task-05
@@ -58,10 +72,8 @@
 
 // let num = +prompt();
 
-// for(let i = num - 1; i > 0; i--){
-//     if(i % 2 !== 0){
-//         console.log(i);
-//     }
+// for (let i = num; i > 0; i--) {
+//     console.log(i);
 // }
 
 
@@ -70,27 +82,36 @@
 // For dövrün köməyi ilə 10-a qədər bütün ədədlərin hasilin tapan proqram tərtib edin.
 // Alınan dəyəri konsola çıxarın.
 
+// let num = +prompt("Введите число:");
+// let sum = 1;
+// for (let i = num; i <= 10; i++) {
+//     sum = sum * i;
+// }console.log(`Произведение чисел от ${num} до 10 = ${sum}`); 
+
+
 
 // Task-08
 // İstifadəçidən mətn daxil etməsini tələb edən və mətn elementlərini əks qaydada düzən proqram tərtib edin.
 
-// let word = prompt().toLowerCase();
-// let reverse = "";
 
-// for(let i = word.length - 1; i >= 0; i--){
-//     reverse = reverse + word[i];
-// }console.log(reverse);
+// let word = String(prompt());
+// let reverse = "";
+// if (typeof word === "string") {
+//     for (let i = word.length - 1; i >=0; i--) {
+//         reverse = reverse + word[i];
+//     }
+//     console.log(reverse);
+// }
 
 
 // Task-09
 // Mətndə "." hərfinin ilk dəfə hansı indeksdə yerləşdiyini tapan proqram tərtib edin.
 
-// let text = prompt().toLowerCase();
-// let index = 0;
+// let word = prompt();
 
-// for(let i = 0; i < text.length; i++){
-//     if(index[i] === "."){
-//         index = i;
+// for (let i = word.length; i >= 0 ; i--) {
+//     if (word[i] === ".") {
+//         console.log("Noqte ilk olaraq " + i + " index de tapildi");
 //         break;
 //     }
 // }

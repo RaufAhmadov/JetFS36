cf545806b48d90de6d9ff0dbbeb3882dac43dbdd
Rauf <104986212+RaufAhmadov@users.noreply.github.com>
2024-12-09 01:51:46 +0400

    Test JS Func 09.12.2024
    Function task from ChatGPT

---
+++ JS/Home Task Func/app.js
@@ -0,0 +1,31 @@
+
+// 1 ci task sözlərin sıralanması 
+
+// function sortedWords(inside){
+//     return inside.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+// }
+
+// let safe = [];
+
+// for(let i = 0; i < 5;i++){
+//     let word = prompt().toLowerCase();
+//     safe.push(word);
+// }
+
+// let getUser = sortedWords(safe);
+// document.write(`Sorted words from array ${getUser.join(",")}`);
+
+
+// 2 ci task Polindrom sözlər
+
+
+// function getCheck(check){
+//     let cleanStr = check.toLowerCase().replace(/\s+/g, "");
+//     let reverseStr = cleanStr.split("").reverse().join("");
+//     return cleanStr === reverseStr;
+// }
+
+// let word = prompt().toLowerCase();
+
+// if(getCheck(word)){document.write("Palindrome");}
+// else{document.write("!Palindrome");}

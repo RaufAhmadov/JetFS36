7bc0ca5eaaf528cd54212a99d803307a89762ea0
Rauf <104986212+RaufAhmadov@users.noreply.github.com>
2024-12-23 02:04:16 +0400

    Home Task JS Array 1
    Task 1-3 is complete

---
+++ JS/Home Task JS Arr/array1/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// 1. Üç qrup yoldaşının adlarından ibarət massiv yaradın; Başqa bir qrup yoldaşının adını massivin sonuna əlavə edin; İlk adı silin; Massivi konsola çıxarın.
+
+// const team201 = ["Eldaniz", "Cavanshir", "Ismayıl", "Cahangir"];
+// team201.shift();
+// team201.push("Fuad");
+
+// document.write(team201);
+
+
+// 2. Üç addan ibarət massiv yaradın. Massivin ikinci elementinin dəyərini “Classified” ilə əvəz edin. Massivi konsola çıxarın.
+
+// const agent = ["Bourne", "Bond", "Hunt"];
+// agent[1] = "Classified";
+
+// document.write(agent);
+
+
+// 3. Massiv verilib. "Episode 4: New Hope" kimi mesajları ardıcıl olaraq konsola çıxarın. Massiv elementlərin düzün.
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+
+// const star_wars = ['New Hope', 'The Empire Strikes Back', 'Return of the Jedi'];
+
+// let episode = 1;
+// for (let movie of star_wars) {
+//     document.write(`Episode ${episode}: ${movie}<br>`);
+//     episode++;
+// }
+
+

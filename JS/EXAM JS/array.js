af05a4caa273a16bf330660eb10224fe1f5be669
Rauf <104986212+RaufAhmadov@users.noreply.github.com>
2025-01-05 23:48:57 +0400

    Exam test Part 2 Start

---
+++ JS/EXAM JS/array.js
@@ -0,0 +1,117 @@
+// 1. Bir array-də, yalnız ədədi 3 ilə bölünən ədədləri çıxarıb qalanları toplamaqla bir nəticə əldə edin.
+
+
+
+// 2. Bir stringdə, həm kiçik, həm də böyük hərflərin neçə dəfə təkrarladığını tapın.
+
+// 3. Bir array-dəki ən böyük iki ədədin cəm və hasilini tapın.
+
+
+// 4. Bir stringdəki bütün böyük hərflərin sayını tapın, ardıcıl olaraq hər hansı kiçik hərflər varsa, onları silin.
+
+// 5. Verilmiş massivdəki bütün elementlərin ortalamasını hesablayın.
+
+// 6. Massivdəki elementlərin yerini qarışdırın (shuffle).
+
+// 7. Verilmiş ədədin ikili sistemdə (binary) yazılışını hesablayın. (internetde axtara bilersen,how to convert integer to binary number javascript)
+
+// 8.Verilmiş ədədin faktorialını hesablayın.
+
+
+
+// 9. Üç qrup yoldaşının adlarından ibarət massiv yaradın; Başqa bir qrup yoldaşının adını massivin sonuna əlavə edin; 
+// İlk adı silin; Massivi konsola çıxarın.
+
+// 10. Üç addan ibarət massiv yaradın. Massivin ikinci elementinin dəyərini “Classified” ilə əvəz edin. Massivi konsola çıxarın.
+
+// 11. Massiv verilib. "Episode 4: New Hope" kimi mesajları ardıcıl olaraq konsola çıxarın. Massiv elementlərin düzün.
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+
+// //Episode 4: New Hope
+// //Episode 5: Empire strikes back
+// //Episode 6: Return of the Jdi
+
+// 12.İstifadəçidən vergüllə ayrılmış beş ədədi daxil etməyi xahiş edin və onları massivdə saxlayın. 
+// İstifadəçidən beş ədəd elementi daxil etməyi tələb edən, onları massivə yazan və ən kiçik ədədi konsola çıxaran proqram yazın.
+
+// 13. Verilən mətn tip dəyərdəki vergülləri nöqtəli vergüllə əvəz edin. '32, 31, 34, 36, 31' mətni verilmişdir 
+// İçindəki vergülləri nöqtəli vergüllə əvəz edin.(Massivden istifadə ederek
+
+// 14. İki array verilib. Bu iki array-i birləşdirin və təkrarlanan elementləri aradan qaldırın.
+
+// 15. İki array verilib. Bu iki array-dən yalnızca ilk array-də olub, ikinci array-də isə olmayan elementləri tapın.
+
+// 16. Tam ədədlər massivini qəbul edən, onu artan ardıcıllıqla sıralayan və sıralanmış massivi geri qaytaran 
+// sortArray() funksiyasını yazın.
+
+// let array = [
+//   1, 6, 34, 5, 1, -4, 54, 76, 23, 65, 3, 50, 45, 100, 2346, -52, 6545, 0, 45, 22,
+// ];
+
+// 17.Rəqəmlər massivini parametr kimi qəbul edən və əgər həmin massivdə mənfi ədədlər varsa onları massiv şəklində geri qaytaran, 
+// yoxdursa uyğun mesajı geri funksiya yazın.
+
+
+
+// 18. Orijinal massivin elementlərindən ibarət yeni massiv yaradın.
+// Rəqəmlər massivi verilmişdir. Aşağıdakı qaydalarla dəyişdirilən orijinal massivin elementlərindən ibarət yeni massiv yaradın:
+
+// mənfi ədədlər müsbət olublar;
+// müsbət ədədlər ikiqat artırılıb.
+// Input:  [1, 5, -7, 3, -9, 4, -6, 2];
+// Output: [2, 10, 7, 6, 9, 8, 6, 4]
+
+// 19. Adlar massivi verilib. Bir funksiya yazın, hansı ki parametr olaraq massiv qəbul edir. Həmin massivdə "A" hərfi ilə başlayan 
+// elementlərdən ibarət yeni massiv yaradıb, elə bir dəyər tapılmasa uyğun mesajı geri qaytarmalıdır.
+
+// let namesArray = [
+//   "Alice",
+//   "Bob",
+//   "Catherine",
+//   "David",
+//   "Eva",
+//   "Andrew",
+//   "Frank",
+//   "Anna",
+//   "George",
+//   "Alex",
+// ];
+
+
+// 20.Bir array-dəki hər bir ədədin 2 qatını alıb yeni bir array yaradın.
+
+// 21.Müəyyən bir ədədin array-də olub olmadığını yoxlayın.
+ 
+// 22.Array-dəki bütün müsbət ədədləri bir array-də toplayın.
+
+// 23.Array-dəki ən kiçik ədədin hansı olduğunu tapın.
+
+// 24.Array-dəki bütün ədədlərin cəmindən böyük olan bir ədəd tapın.
+
+// 25.Bir array-dəki bütün ədədlərin kvadratını alıb yeni bir array yaradın.
+
+// 26.Array-dəki heç olmasa bir ədədin 5-ə bölünməsini yoxlayın.
+
+// 27.Array-dəki bütün ədədlərin müsbət olduğunu yoxlayın.
+
+// 28.Array-dəki sözlərdən yalnız uzunluğu 5 və daha çox olanları seçin.
+
+// 29.Array-dəki ən böyük ədədin tapılmasını təmin edin.
+
+// 30.Array-dəki ən kiçik ədədin tapıldığı halda həmin ədədi silin.
+
+// 31.Array-də "JavaScript" sözünü tapın və tapıldıqda həmin sözün yerini dəyişdirin.
+
+// 32.Array-dəki bütün adları kiçik hərflərlə yazın.
+
+// 33.Bir array-də olan hər bir elementin 3 ilə vurulmuş formasını yaradın.
+
+// 34.Array-dəki cüt ədədləri filtrləyin və yeni bir array yaradın.
+
+// 35.Array-dəki sözlərin uzunluğuna əsasən onları artan sıraya düzün.
+
+// 36.Array-də ən azı bir ədədin 3-ə bölünən olub olmadığını yoxlayın.
+
+// 37.Array-dəki bütün elementlərin tərsini qaytarın.
+
+// 38.Array-dəki müəyyən bir sözü tapın və o sözü dəyişdirin.

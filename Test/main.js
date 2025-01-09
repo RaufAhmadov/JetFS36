c619c52dd532a8edda7736ce699dc8a77c9d3437
Rauf <104986212+RaufAhmadov@users.noreply.github.com>
2025-01-10 02:23:06 +0400

    Testing DOM

---
+++ Test/main.js
@@ -0,0 +1,52 @@
+"use strict"
+
+
+// const get_header = document.querySelectorAll(".header");
+
+// for(let changed_head of get_header){
+//     changed_head.style.color = "red";
+// }
+
+
+// const nodeList = document.querySelectorAll(".header , .content");
+// nodeList.forEach(
+//     function(el){
+//         console.log(el.textContent);
+//     }
+// )
+
+
+// const get_header = document.querySelector("#header");
+// const get_main = document.querySelector("#main");
+// const get_content = document.querySelector("#content");
+// const get_footer = document.querySelector("#footer");
+// const get_btn = document.querySelector("#change_btn");
+
+
+// get_btn.onclick = function(){
+//     const backgroundColor = header.getAttribute("header");
+
+//     if(backgroundColor === "green"){ header.setAttribute(".header" , ".red"); }
+//     else if(backgroundColor === "blue"){ content.setAttribute(".main" , ".green");}
+//     else if(backgroundColor === "yellow"){ content.setAttribute(".content" , ".yellow");}
+//     else if(backgroundColor === "red"){ footer.setAttribute(".footer", ".blue")}
+// }
+
+
+const get_header = document.querySelector("#header");
+const get_main = document.querySelector("#main");
+const get_content = document.querySelector("#content");
+const get_footer = document.querySelector("#footer");
+const get_btn = document.querySelector("#change_btn");
+
+get_btn.onclick = function(){
+    const result = 0;
+    for(let x of){}
+        get_header.setAttribute("class","red");
+        get_main.setAttribute("class","green");
+        get_content.setAttribute("class","yellow");
+        get_footer.setAttribute("class","blue");
+}
+
+
+

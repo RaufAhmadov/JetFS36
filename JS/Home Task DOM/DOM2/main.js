9923817c0e73efb121d86083fb11ef60b29b823b
Rauf <104986212+RaufAhmadov@users.noreply.github.com>
2025-01-12 03:45:02 +0400

    update

---
+++ JS/Home Task DOM/DOM2/main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const open_btn = document.querySelector("open_btn");
+const close_btn = document.querySelector("close_btn");
+
+let bars_open = false;
+
+push_btn.onclick = function(){
+    open_btn.setAttribute("" )
+}

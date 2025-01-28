409ae963e63f931f3d7f0e6a821dbcb2022e2c40
Rauf <104986212+RaufAhmadov@users.noreply.github.com>
2025-01-28 21:06:49 +0400

    update

--- JS/Home Task POST,GET,Delete/data.js
+++ JS/Home Task POST,GET,Delete/data.js
@@ -2,88 +2,93 @@
 
 const getUserList = document.querySelector("#user_list");
 const getCountryFilter = document.querySelector("#countryFilter");
+const connect = "http://localhost:3000/usersList";
 
 
 // USERS AND CATEGOTY LISTS
 const countryCategory = ["All", "Azerbaijan", "Russian", "USA"]
-const usersList = [
-    {
-        "id": "",
-        "name": "Leanne",
-        "surname": "Bret",
-        "email": "Sincere@april.biz",
-        "country": "Azerbaijan" ,
-        "address": "Light str 123" ,
-        "contact": "1-770-736-8031",
-        "website": "hildegard.org",
-    },
-    {
-        "id": "",
-        "name": "Ervin",
-        "surname": "Howell",
-        "email": "Shanna@melissa.tv",
-        "country": "Azerbaijan" ,
-        "address": "Light str 123" ,
-        "contact": "1-770-736-8031",
-        "website": "hildegard.org",
-    },
-    {
-        "id": "",
-        "name": "Clementine",
-        "surname": "Bauch",
-        "email": "Nathan@yesenia.net",
-        "country": "Azerbaijan" ,
-        "address": "Light str 123" ,
-        "contact": "1-770-736-8031",
-        "website": "hildegard.org",
-    },
-    {
-        "id": "",
-        "name": "Patricia",
-        "surname": "Lebsack",
-        "email": "Julianne.OConner@kory.org",
-        "country": "Azerbaijan" ,
-        "address": "Light str 123" ,
-        "contact": "1-770-736-8031",
-        "website": "hildegard.org",
-    },
-    {
-        "id": "",
-        "name": "Chelsey",
-        "surname": "Dietrich",
-        "email": "Lucio_Hettinger@annie.ca",
-        "country": "Azerbaijan" ,
-        "address": "Light str 123" ,
-        "contact": "1-770-736-8031",
-        "website": "hildegard.org",
-    },
-];
 
-// SHOW USERS
-const showUsers = (arr) => {
-    getUserList.innerHTML = "";
-    arr.forEach(({id , name , surname , email , country, address, contact, website, img}) => {
-        getUserList.innerHTML += `
-        <tr>
-            <td>${id}</td>
-            <td>${name}</td>
-            <td>${surname}</td>
-            <td>${email}</td>
-            <td>${country}</td>
-            <td>${address}</td>
-            <td>${contact}</td>
-            <td>${website}</td>
-            <td>
-            <button class="edit_btn">Edit</button>
-            <button class="del_btn">Delete</button>
-            </td>
-            <!-- <td><img src="${img}" alt="User Image" style="width: 50px; height: 50px;"></td> -->
-        </tr>`;
-    });
-};
-showUsers(usersList);
+
+// const usersList = [
+// //     {
+// //         "id": "",
+// //         "name": "Leanne",
+// //         "surname": "Bret",
+// //         "email": "Sincere@april.biz",
+// //         "country": "Azerbaijan" ,
+// //         "address": "Light str 123" ,
+// //         "contact": "1-770-736-8031",
+// //         "website": "hildegard.org",
+// //     },
+// //     {
+// //         "id": "",
+// //         "name": "Ervin",
+// //         "surname": "Howell",
+// //         "email": "Shanna@melissa.tv",
+// //         "country": "Azerbaijan" ,
+// //         "address": "Light str 123" ,
+// //         "contact": "1-770-736-8031",
+// //         "website": "hildegard.org",
+// //     },
+// //     {
+// //         "id": "",
+// //         "name": "Clementine",
+// //         "surname": "Bauch",
+// //         "email": "Nathan@yesenia.net",
+// //         "country": "Azerbaijan" ,
+// //         "address": "Light str 123" ,
+// //         "contact": "1-770-736-8031",
+// //         "website": "hildegard.org",
+// //     },
+// //     {
+// //         "id": "",
+// //         "name": "Patricia",
+// //         "surname": "Lebsack",
+// //         "email": "Julianne.OConner@kory.org",
+// //         "country": "Azerbaijan" ,
+// //         "address": "Light str 123" ,
+// //         "contact": "1-770-736-8031",
+// //         "website": "hildegard.org",
+// //     },
+// //     {
+// //         "id": "",
+// //         "name": "Chelsey",
+// //         "surname": "Dietrich",
+// //         "email": "Lucio_Hettinger@annie.ca",
+// //         "country": "Azerbaijan" ,
+// //         "address": "Light str 123" ,
+// //         "contact": "1-770-736-8031",
+// //         "website": "hildegard.org",
+// //     },
+// // ];
+
+// // SHOW USERS
+// // const showUsers = (arr) => {
+// //     getUserList.innerHTML = "";
+// //     arr.forEach(({id , name , surname , email , country, address, contact, website, img}) => {
+// //         getUserList.innerHTML += `
+// //         <tr>
+// //             <td>${id}</td>
+// //             <td>${name}</td>
+// //             <td>${surname}</td>
+// //             <td>${email}</td>
+// //             <td>${country}</td>
+// //             <td>${address}</td>
+// //             <td>${contact}</td>
+// //             <td>${website}</td>
+// //             <td>
+// //             <button class="edit_btn">Edit</button>
+// //             <button class="del_btn">Delete</button>
+// //             </td>
+// //             <!-- <td><img src="${img}" alt="User Image" style="width: 50px; height: 50px;"></td> -->
+// //         </tr>`;
+// //     });
+// // };
+// showUsers(usersList);
 
 // SHOW COUNTRY FILTER
+
+
 const showCountryFilter = (arr) => {
     getCountryFilter.innerHTML = "";
     arr.forEach((country) => {
@@ -93,3 +98,6 @@ const showCountryFilter = (arr) => {
     });
 };
 showCountryFilter(countryCategory);
+
+
+axios.get(connect).then((res) => {console.log(res);});
